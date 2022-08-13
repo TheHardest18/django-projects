8339c64fc8558d30f287b3e83db8c53cd1763c95
TheHardest18 <isias1626@gmail.com>
2022-08-13 12:19:24 -0400

    [ADD] new class and filters in Admin.py and change language in settings.py

--- TiendaOnline/GestionPedidos/admin.py
+++ TiendaOnline/GestionPedidos/admin.py
@@ -4,7 +4,16 @@ from GestionPedidos.models import Clientes, Articulos, Pedidos
 class ClientesAdmin(admin.ModelAdmin):
     list_display = ("nombre", "direccion", "email", "telefono")
     search_fields = ("nombre", "telefono")
+
+class ArticulosAdmin(admin.ModelAdmin):
+    list_filter = ("seccion",)
+
+class PedidosAdmin(admin.ModelAdmin):
+    list_display = ("numero", "fecha")
+    search_fields = ("numero",)
+    list_filter = ("fecha",)
+    date_hierarchy = "fecha"
 admin.site.register(Clientes, ClientesAdmin)
-admin.site.register(Articulos)
-admin.site.register(Pedidos)
+admin.site.register(Articulos, ArticulosAdmin)
+admin.site.register(Pedidos, PedidosAdmin)
 # Register your models here.
