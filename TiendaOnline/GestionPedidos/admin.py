3e1d1255af844e80c2ca2f9d06f66ccaa5837bb0
TheHardest18 <isias1626@gmail.com>
2022-08-13 11:30:33 -0400

    [ADD] edit Admin.py and notes

--- TiendaOnline/GestionPedidos/admin.py
+++ TiendaOnline/GestionPedidos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from GestionPedidos.models import Clientes, Articulos, Pedidos
 
+admin.site.register(Clientes)
+admin.site.register(Articulos)
+admin.site.register(Pedidos)
 # Register your models here.
