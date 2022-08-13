dcea9d441150d8f0dd0d94dc3d397730f979ca63
TheHardest18 <isias1626@gmail.com>
2022-08-13 17:26:22 -0400

    [ADD] new form

--- TiendaOnline/GestionPedidos/views.py
+++ TiendaOnline/GestionPedidos/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from GestionPedidos.models import Articulos
 # Create your views here.
 def busqueda_productos(request):
     return render(request, "busqueda_productos.html")
 
 def buscar(request):
-    mensaje ="articulo buscado: %r" %request.GET["prd"]
+    if request.GET["prd"]:
+        producto = request.GET["prd"]
+        if len(producto) > 20:
+            mensaje = "Texto de busqueda excede limite establecido"
+        else:
+            articulos = Articulos.objects.filter(nombre__icontains=producto)
+            return render(request, "resultados_busqueda.html", {"articulos": articulos, "query": producto})
+        # mensaje = "articulo buscado: %r" %request.GET["prd"]
+
+    else:
+        mensaje = "No has introducido nada"
+
     return HttpResponse(mensaje)
+def contacto(request):
+    if request.method=="POST":
+        return render(request, "gracias.html")
+    return render(request, "contacto.html")
