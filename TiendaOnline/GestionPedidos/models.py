35b15eaa0d1c456e4f64132159e9aad2c4fb3d1e
TheHardest18 <isias1626@gmail.com>
2022-08-13 11:48:28 -0400

    [ADD] verbose

--- TiendaOnline/GestionPedidos/models.py
+++ TiendaOnline/GestionPedidos/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Clientes(models.Model):
-    nombre = models.CharField(max_length=30)
+    nombre = models.CharField(max_length=30, verbose_name="Cliente") #verbose_name is change in panel admin
     direccion = models.CharField(max_length=50)
     email = models.EmailField(blank=True, null=True)
     telefono = models.CharField(max_length=10)
+    def __str__(self):
+        return 'nombre: %s\n' % (self.nombre)
 
 class Articulos(models.Model):
     nombre = models.CharField(max_length=30)
@@ -18,3 +20,5 @@ class Pedidos(models.Model):
     numero = models.IntegerField()
     fecha = models.DateField()
     entregado = models.BooleanField()
+    def __str__(self):
+        return 'No. Pedido: %s\n Estado: %s' % (self.nombre, self.entregado)
