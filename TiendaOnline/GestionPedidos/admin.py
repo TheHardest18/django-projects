dcb99345231e2267f987e43e80f137800406b46b
TheHardest18 <isias1626@gmail.com>
2022-08-13 11:50:07 -0400

    [ADD] verbose_name for admin panel and List_display

--- TiendaOnline/GestionPedidos/admin.py
+++ TiendaOnline/GestionPedidos/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from GestionPedidos.models import Clientes, Articulos, Pedidos
 
-admin.site.register(Clientes)
+class ClientesAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "direccion", "email", "telefono")
+    search_fields = ("nombre", "telefono")
+admin.site.register(Clientes, ClientesAdmin)
 admin.site.register(Articulos)
 admin.site.register(Pedidos)
 # Register your models here.
