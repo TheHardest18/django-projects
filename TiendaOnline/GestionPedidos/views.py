8431452b62d0ff614c0e80535f5a5111a7cb6dcf
TheHardest18 <isias1626@gmail.com>
2022-08-15 22:17:26 -0400

    [ADD] new form view

--- TiendaOnline/GestionPedidos/views.py
+++ TiendaOnline/GestionPedidos/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from GestionPedidos.models import Articulos
+from django.core.mail import send_mail
+from django.conf import settings
 # Create your views here.
 def busqueda_productos(request):
     return render(request, "busqueda_productos.html")
@@ -21,5 +23,11 @@ def buscar(request):
     return HttpResponse(mensaje)
 def contacto(request):
     if request.method=="POST":
+        subject = request.POST["asunto"]
+        message = request.POST["mensaje"]
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = ["isias1626@gmail.com"]
+        send_mail(subject, message, email_from, recipient_list)
+
         return render(request, "gracias.html")
     return render(request, "contacto.html")
